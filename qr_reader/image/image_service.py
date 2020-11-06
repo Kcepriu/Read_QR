5b4ad78ce0fe3c5de9a9fd863632d4a2c3d6beac
sergiy <kcepriu@gmail.com>
2020-11-06 22:33:20 +0200

    image d

--- qr_reader/image/image_service.py
+++ qr_reader/image/image_service.py
@@ -9,7 +9,7 @@ class IMAGE_PR:
         if filename:
             self.img = cv2.imread(filename)
         elif byte_string:
-            nparr = np.fromstring(byte_string, np.uint8)
+            nparr = np.frombuffer(byte_string, np.uint8)
             self.img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
         else:
             self.img = None
@@ -29,53 +29,43 @@ class IMAGE_PR:
             return None
 
 
-    def return_image_corner(self, _img, type_corner):
-        if type_corner == self.TOP_LEFT:
-            return _img[0:int(self.h * 0.25), 0:int(self.w*0.2)]
-
-        elif type_corner == self.TOP_RIGHT:
-            return _img[0:int(self.h * 0.2), int(self.w * 0.75):self.w]
-
-        elif type_corner == self.LOWER_LEFT:
-            (h, w, d) = _img.shape
-            center = (w // 2, h // 2)
-            M = cv2.getRotationMatrix2D(center, 180, 1.0)
-            new_img = cv2.warpAffine(_img, M, (w, h))
-            return self.return_image_corner(new_img, self.TOP_RIGHT)
-
-        elif type_corner == self.LOWER_RIGHT:
-            (h, w, d) = _img.shape
-            center = (w // 2, h // 2)
-            M = cv2.getRotationMatrix2D(center, 180, 1.0)
-            new_img = cv2.warpAffine(_img, M, (w, h))
-            return self.return_image_corner(new_img, self.TOP_LEFT)
-
-        else:
-            return self.img
+    def return_image_corner(self):
+        return self.img[0:int(self.h * 0.2), int(self.w * 0.75):self.w]
 
     def auto_find_qr_code(self):
-        for type_corner in  self.TYPES_CORNER:
-            # print(type_corner)
-            new_img = self.return_image_corner(self.img, type_corner)
+        if self.w > self.h:
+            self.img = cv2.rotate(self.img, cv2.ROTATE_90_COUNTERCLOCKWISE ) #ROTATE_90_CLOCKWISE)
+            (self.h, self.w, self.d) = self.img.shape
 
-            data_barcode = self.decode_barcodes(new_img)
-            # self.show_img(new_img)
-            if data_barcode:
-                print(data_barcode)
-                break
+        data_barcode = self.decode_barcodes(self.return_image_corner())
+
+        if data_barcode:
+            return data_barcode
+
+        self.img = cv2.rotate(self.img, cv2.ROTATE_180)
+        data_barcode = self.decode_barcodes(self.return_image_corner())
+
         return data_barcode
 
-    def show_img(self, _img):
+    def resize_scan(self):
+        if self.h > 1280:
+            coef = 1280 / self.h
+            self.img = cv2.resize(self.img, (0,0), fx=coef, fy=coef)
+            (self.h, self.w, self.d) = self.img.shape
+
+
+    @staticmethod
+    def show_img(_img):
         cv2.imshow("Image", _img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
 if __name__ == '__main__':
 
-    fd = open('../../test/IMG/qqq.jpg', "rb")
+    fd = open('../../test/IMG/ddd.jpg', "rb")
     img_str = fd.read()
     fd.close()
-    print(1)
+
     qr_r = IMAGE_PR(byte_string=img_str)
 
     # qr_r = IMAGE_PR('../../test/IMG/ddd.jpg')
@@ -84,5 +74,10 @@ if __name__ == '__main__':
 
     print(qr_code)
 
+    print(qr_r.h, qr_r.w)
+
     # qr_r.show_img(qr_r.return_image_corner(qr_r.img, qr_r.TOP_LEFT) )
+    qr_r.resize_scan()
+    print(qr_r.h, qr_r.w)
+    qr_r.show_img(qr_r.img)
 
